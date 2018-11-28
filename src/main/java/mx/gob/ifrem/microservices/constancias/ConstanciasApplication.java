ddfcf4df2717ed812181f2febcd9d45ff52e4ec6
ALEJANDRO LARA GTZ <alara@MacBook-Pro-de-ALEJANDRO.local>
2018-11-28 14:46:30 -0600

    inicio de micro

---
+++ src/main/java/mx/gob/ifrem/microservices/constancias/ConstanciasApplication.java
@@ -0,0 +1,12 @@
+package mx.gob.ifrem.microservices.constancias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstanciasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConstanciasApplication.class, args);
+    }
+}
